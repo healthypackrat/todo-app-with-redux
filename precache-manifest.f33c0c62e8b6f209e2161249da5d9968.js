80d27645dcde92f191c8e28fd0d8005f1a6524a9
healthypackrat <healthypackrat@gmail.com>
2019-07-25 20:02:24 +0900

    Updates

---
+++ precache-manifest.f33c0c62e8b6f209e2161249da5d9968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f7270e7f9477169a81f1007cc54077",
+    "url": "/todo-app-with-redux/index.html"
+  },
+  {
+    "revision": "fa832b0a201d2a283fd3",
+    "url": "/todo-app-with-redux/static/css/main.a14c3943.chunk.css"
+  },
+  {
+    "revision": "57f7326023343b84b1e3",
+    "url": "/todo-app-with-redux/static/js/2.71d9bb47.chunk.js"
+  },
+  {
+    "revision": "fa832b0a201d2a283fd3",
+    "url": "/todo-app-with-redux/static/js/main.00349017.chunk.js"
+  },
+  {
+    "revision": "a0dd3857ed2fee74c69d",
+    "url": "/todo-app-with-redux/static/js/runtime~main.12f6794d.js"
+  }
+]);
