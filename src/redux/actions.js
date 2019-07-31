fcce4f61a955bc98abba9cdc7b6e2c6704374cac
healthypackrat <healthypackrat@gmail.com>
2019-08-01 05:44:35 +0900

    Remove unused codes for setting filters

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,5 @@
 import uuid from "uuid/v4";
-import { ADD_TODO, TOGGLE_TODO, DELETE_TODO, SET_FILTER } from "./actionTypes";
+import { ADD_TODO, TOGGLE_TODO, DELETE_TODO } from "./actionTypes";
 
 export const addTodo = content => ({
   type: ADD_TODO,
@@ -18,5 +18,3 @@ export const deleteTodo = id => ({
   type: DELETE_TODO,
   payload: { id }
 });
-
-export const setFilter = filter => ({ type: SET_FILTER, payload: { filter } });
